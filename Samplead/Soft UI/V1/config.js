fb4aed5f66c337f8cd657201d2b30928704328d9
GEDstudios <53018342+GEDstudios@users.noreply.github.com>
2026-04-08 22:29:44 +0300

    Add files via upload

---
+++ Samplead/Soft UI/V1/config.js
@@ -0,0 +1,15 @@
+export const config = {
+    type: "project",
+    title: "Saemplead Soft UI",
+    description: "Animations displaying the user interface and interactions.",
+    navDepth: "../../../",
+    
+    sections: [
+        {
+            title: "Soft UI",
+            files: [
+                { fileName: "UI_1_Define_ICP.lottie", loop: true, fullWidth: true},
+            ]
+        },
+    ]
+};
---
+++ Samplead/landing-config.js
@@ -0,0 +1,28 @@
+export const config = {
+    type: "landing",
+    clientName: "Samplead",
+    navDepth: "../", 
+    
+    theme: {
+        brand: "#F45326",            
+        pageBg: "#030304",           
+        cardBg: "rgba(20,20,22,0.6)", 
+        lottieBg: {
+            dark: "rgba(244,238,236,1)", 
+            light: "rgba(255,255,255,1)"
+        }
+    },
+
+    projects: [
+        {
+            category: "Soft UI",
+            main: {
+                tag: "Soft UI",
+                badge: "",
+                desc: "Animations displaying the user interace and interactions.",
+                path: "Soft UI/V1/index.html" 
+            },
+            archives: []
+        }
+    ]
+};
