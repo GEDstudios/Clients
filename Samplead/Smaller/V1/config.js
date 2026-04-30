fcc6992838873c5830c61672cdcd19bd945c4a41
GEDstudios <53018342+GEDstudios@users.noreply.github.com>
2026-04-30 14:31:30 +0300

    Add files via upload

---
+++ Samplead/Smaller/V1/config.js
@@ -0,0 +1,17 @@
+export const config = {
+    type: "project",
+    title: "Saemplead Icons V1",
+    description: "Lottie preview of the icon animations.",
+    navDepth: "../../../",
+    
+    sections: [
+        {
+            title: "Icons",
+            files: [
+                { fileName: "Hero_Smaller.lottie", loop: true, fullWidth: true},
+                { fileName: "Speared Ball_Smaller.lottie", loop: true,fullWidth: true},
+                { fileName: "Footer_Smaller.lottie", loop: true, fullWidth: true},
+            ]
+        },
+    ]
+};
